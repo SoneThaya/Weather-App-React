5f8715c59994785c7488113d5997336a8c5d8442
Sone <sthaya@gmail.com>
2020-03-15 13:24:23 -0700

    made weather card component responsive, card changes color depending on temp.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import WeatherCard from './components/WeatherCard/Component';
 function App() {
 	return (
 		<div className="App">
-			<WeatherCard />
+			<WeatherCard temp="28" />
+			<WeatherCard temp="10" />
+			<WeatherCard temp="40" />
 		</div>
 	);
 }
