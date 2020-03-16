89cba271ae63adb077cb029457d09f07eb52bf7c
Sone <sthaya@gmail.com>
2020-03-15 19:39:15 -0700

    created async await function to connect to open weather api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React from 'react';
 import './App.css';
 import WeatherCard from './components/WeatherCard/Component';
 
+const key = process.env.REACT_APP_KEY
+
 function App() {
+
+	const data = async () => {
+		const apiRes = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,gb&units=imperial&APPID=${key}`);
+		const resJSON = await apiRes.json();
+		return resJSON;
+
+	}
+
+	data().then(res => {
+		console.log(res.main.feels_like)
+		console.log(res.main.temp)
+	})
+
 	return (
 		<div className="App">
 			<WeatherCard temp={-10} condition="Clear" city='Sydney' country='AU' />
